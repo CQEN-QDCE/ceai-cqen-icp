d942cceb3bb0a7702e4d638f632efb10fe34c063
Julio Cesar Torres dos Santos <julio.cesartorres@mcn.gouv.qc.ca>
2025-07-23 13:43:42 -0400

    [func] test run debug 03
    Signed-off-by: Julio Cesar Torres dos Santos <julio.cesartorres@mcn.gouv.qc.ca>

--- argocd/apps/backend.tf
+++ argocd/apps/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket               = "x-road-preprod-github-action-ressources"
     workspace_key_prefix = "environments"
-    key                  = "projects/base/icp/terraform.tfstate"
+    key                  = "xroad-k8s/terraform.tfstate"
     region               = "ca-central-1"
     profile              = "xroad-preprod"   
   }
